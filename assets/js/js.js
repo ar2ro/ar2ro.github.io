1b9b08a6e288efc5f718797cdbcab74636fd7d4e
Arturo Montero <arturo@ar2.ro>
2018-07-24 11:59:44 -0400

    redesign site

---
+++ assets/js/js.js
@@ -0,0 +1,7 @@
+//Add "_blank" target to any 
+//var links = document.getElementsByTagName('a');
+//for(var i = 0; i < links.length; i++) {
+//	if(!links[i].href.startsWith('#')){
+//		links[i].target = "_blank"
+//	}
+//}
