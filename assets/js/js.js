fcd3a75e8101c2fdaa0f566ddf350a0ca9637d3b
Arturo Montero <arturo@ar2.ro>
2018-07-29 20:24:07 -0400

    add icon to anchors to other domains

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,7 +1,5 @@
-//Add "_blank" target to any 
-//var links = document.getElementsByTagName('a');
-//for(var i = 0; i < links.length; i++) {
-//	if(!links[i].href.startsWith('#')){
-//		links[i].target = "_blank"
-//	}
-//}
+$('.page-content a').each(function(i,v){
+	if($(v).attr('href').startsWith('http')){
+		$(v).attr('target', '_blank')
+	}
+})
